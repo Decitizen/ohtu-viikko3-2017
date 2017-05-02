16151ad87d1e8e4a20f8e1d812490aa3bb92ce75
Ville Saarinen <ville.saarinen@helsinki.fi>
2017-05-02 14:10:57 +0300

    teht 3

--- QueryLanguage/src/main/java/statistics/Main.java
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
           
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
+        Matcher m = new Or( 
+                        new And(
+                            new HasFewerThan(12, "assists"), new PlaysIn("CHI")),
+                        new And(new HasAtLeast(30, "goals"), new Not(new PlaysIn("SJS")))
         );
         
         for (Player player : stats.matches(m)) {
